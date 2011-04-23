8d29f02127a38ed4a37e3c5b35254c1bc6b73600
thachln <thachln@8c9dc9ed-9053-dc3b-3cd5-44c8541756d8>
2011-04-23 04:23:43 +0000

    Update BaseOutForm.java: + Support providing next screen of the control + Support send redirect in control instead of in MVC Core

--- CoreWa-Config/src/openones/corewa/BaseOutForm.java
+++ CoreWa-Config/src/openones/corewa/BaseOutForm.java
@@ -4,23 +4,47 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+import openones.corewa.config.Event;
+
 public class BaseOutForm implements Serializable {
     private Map<String, Object> requestMap = new HashMap<String, Object>();
     private Map<String, Object> sessionMap = new HashMap<String, Object>();
     private BaseInForm inForm;
-    
+    private String nextScreen;
+    /** transType: FORWARE, INCLUDE. Null means redirect */
+    private Event.DispType dispType;
+
+    /** Processed request dispatch in sub control. */
+    private boolean isDispatched;
+
     public void putRequest(String key, Object value) {
         requestMap.put(key, value);
     }
-    
+
+    public Event.DispType getDispType() {
+        return dispType;
+    }
+
+    public void setDispType(Event.DispType dispType) {
+        this.dispType = dispType;
+    }
+
+    public boolean isDispatched() {
+        return isDispatched;
+    }
+
+    public void setDispatched(boolean isDispatched) {
+        this.isDispatched = isDispatched;
+    }
+
     public void putSession(String key, Object value) {
         sessionMap.put(key, value);
     }
-    
+
     public Map<String, Object> getRequestMap() {
         return requestMap;
     }
-    
+
     public Map<String, Object> getSessionMap() {
         return sessionMap;
     }
@@ -31,6 +55,14 @@ public class BaseOutForm implements Serializable {
 
     public void setInForm(BaseInForm inForm) {
         this.inForm = inForm;
+    }
+
+    public String getNextScreen() {
+        return nextScreen;
+    }
+
+    public void setNextScreen(String nextScreen) {
+        this.nextScreen = nextScreen;
     }
 
 }
